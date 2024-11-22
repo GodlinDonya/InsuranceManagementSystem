36c0341588f82ac1dd370db9cbb4a361635af947
GodlinDonya <140385097+GodlinDonya@users.noreply.github.com>
2024-11-22 13:53:43 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Insurance_Management_System.App;
+
+namespace Insurance_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           InsuranceApp app = new InsuranceApp();
+            app.Run();
+        }
+    }
+}
